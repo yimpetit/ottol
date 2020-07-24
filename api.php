bac5beb9496e9ebe80f5dd6f5844a4b64e6bade9
임재균 <yjg@deali.net>
2020-07-24 15:53:59 +0900

    a

---
+++ api.php
@@ -0,0 +1,24 @@
+<?php
+ 
+        $url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903";
+ 
+        $is_post = false;
+        $ch = curl_init();
+ 
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, $is_post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $response = curl_exec($ch);
+        
+        $data=json_decode($response,true);
+
+        
+
+
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+ 
+        echo json_encode($data);
+
+?>
